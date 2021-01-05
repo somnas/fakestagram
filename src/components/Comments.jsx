3e74168e43dd8a39705689a4a0ad97aef7172c41
somnas <erik.somnas@gmail.com>
2021-01-05 18:05:48 +0100

    add useEffect to fetch data

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -10,8 +10,6 @@ export default function Comments() {
             addComment([...commentList, input.value]);
             input.value = "";
         }        
-        
-        console.log(commentList);
     }
         
     return (        
--- src/pages/ImageListPage.jsx
+++ src/pages/ImageListPage.jsx
@@ -1,19 +1,19 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import ImageItem from '../components/ImageItem'
 
 
 export default function ImageListPage() {          
     const [imageList, setImageList] = useState([])
 
-    function handleOnClick() {
+    useEffect(() => {
         fetch("https://image-mock-data.firebaseio.com/images.json")
         .then(res => res.json())
         .then(data => setImageList(data))
-    }
+    },[])
+
     return (
         <>
-            <h2>Instagram</h2>
-            <button onClick={handleOnClick} className="btn btn-primary">Load Data</button>
+            <h2>Instagram</h2>            
             <div className="row">
                 {imageList.map(imageItem => {
                 return <ImageItem key={imageItem.id} imageItem={imageItem} />
